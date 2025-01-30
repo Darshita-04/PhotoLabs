9a4dad5c29cdf82fd1481e96de52bb242ee13da8
Darshita Patel <pateldarshita1994@gmail.com>
2025-01-30 08:28:33 -0500

    PhotoListItem Component Cretaed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,27 @@ import React from 'react';
 
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
-
+const sampleDataForPhotoListItem = {
+  id: "1",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+};
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem/>
+      <PhotoListItem 
+      location={sampleDataForPhotoListItem.location}
+      imageSource={sampleDataForPhotoListItem.imageSource}
+      username={sampleDataForPhotoListItem.username}
+      id={sampleDataForPhotoListItem.id}
+      profile={sampleDataForPhotoListItem.profile}
+      />
     </div>
   );
 };

11046eb4ef25118cf508021b46ef1ab644a20859
Darshita Patel <pateldarshita1994@gmail.com>
2025-02-01 04:31:07 -0500

    user can like the pictures. Like functionality implemented

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,11 +3,12 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton() {
+function PhotoFavButton({handleClick, displayAlert, selected}) {
+
   return (
     <div className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <FavIcon handleClick={handleClick} selected={selected} displayAlert={displayAlert} className="photo-list__fav-icon" />
       </div>
     </div>
   );
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "../styles/PhotoListItem.scss";
-
-
+import PhotoFavButton from "./PhotoFavButton";
 
 
 const PhotoListItem = ({photo}) => {
+  const [selected, setSelected] = useState(false);
+  const handleClick = () => {
+    selected ? setSelected(false) : setSelected(true);
+  }
   const { urls, user, location } = photo;
   const { regular:photoUrl, full } = urls || {};
   const { name, profile } = user || {};
   const { city, country } = location || {};
   return (
     <div className="photo-list__item">
+      
+      <PhotoFavButton handleClick={handleClick} selected={selected}/>
       {photoUrl && <img src={photoUrl} alt="..." className="photo-list__image" /> }
       <div className="photo-list__user-details">
        {profile && <img className="photo-list__user-profile" src={profile} alt="..." /> }
